76a18395b3a66e9df60749d5365728f7a184dfa2
Marek Šneberger <marek@sneberger.cz>
2026-02-13 23:23:47 +0100

    feat(nvim): Better go config

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -4,19 +4,18 @@ return {
     opts = {
       servers = {
         golangci_lint_ls = {
-          settings = {
-            golangci_lint_ls = {
-              cmd = { "golangci-lint-langserver" },
-            },
+          cmd = { "golangci-lint-langserver" },
+          init_options = {
+            command = { "golangci-lint", "run", "--output.json.path", "stdout", "--show-stats=false" },
           },
         },
         gopls = {
           settings = {
             gopls = {
-              staticcheck = {
-                -- Disable ST1000 (wrong case in package name) and ST1001 (dot imports)
-                -- since you're using golangci-lint for these checks
-                checks = { "ST1000", "ST1001" },
+              staticcheck = true,
+              analyses = {
+                ST1000 = false,
+                ST1001 = false,
               },
             },
           },
